e904dc77e9f14666321d6fd25c231f873c075e26
abarlow <alexbarlow214@gmail.com>
2023-08-19 21:18:09 -0400

    entires span width and have 3 fields

--- src/pages/enterWorkout.tsx
+++ src/pages/enterWorkout.tsx
@@ -19,128 +19,99 @@ import FitnessCenterOutlinedIcon from "@mui/icons-material/FitnessCenterOutlined
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { borderColor } from "@mui/system";
-import AddCircleSharpIcon from '@mui/icons-material/AddCircleSharp';
+import AddCircleSharpIcon from "@mui/icons-material/AddCircleSharp";
 
-
-
-function addExercise(){
-  return (<Box><Typography>yo</Typography></Box>) 
-}
-
-const ExerciseFields = (id,name) => {
-  const [sets, setSets] = useState([""])
-  return (<Box>
-          {sets.map((item) => (
-            <Typography>aaaa</Typography>
-          ))}
-          <Button
-            // flex="1"
-            // align="right"
-            // sx={{
-            //   color: "black",
-            //   backgroundColor: colors.greenAccent[600],
-            //   borderRadius: 2,
-            //   borderColor: colors.greenAccent[600],
-            //   typography: "h4",
-            //   fontWeight: "bold",
-            //   '&:hover': {
-            //     color: colors.greenAccent[600],
-            //     backgroundColor: "white", 
-            //     borderColor:  colors.greenAccent[600]
-            //   },
-            // }}
-            startIcon={<AddCircleSharpIcon />}
-            onClick={() => {
-              setSets(sets.concat(""))
-            }}
-           variant="outlined"
-          >
-            Newa
-          </Button>
-        </Box>)
-}
-export default function enterWorkout(){
-
+const ExerciseFields = (id, name) => {
+  const [sets, setSets] = useState([""]);
+  return (
+    <Box flex="1" mb="15px">
+      {sets.map((item) => (
+        <Box display="flex" flex="1" sx={{ flexDirection: "row", }} m="10px">
+          <Typography flex="0" align="center" ml="5px" mr="5px">
+            Set
+          </Typography>
+          <Typography flex="0.5" align="center" ml="5px" mr="5px" sx={{border: 1}}>
+            aaaa
+          </Typography>
+          <Typography flex="0.5" align="center" ml="5px" mr="5px" sx={{border: 1}}>
+            bbbb
+          </Typography>
+        </Box>
+      ))}
+      <Button
+        flex="1"
+        align="center"
+        justifyContent="center"
+        // sx={{
+        //   color: "black",
+        //   backgroundColor: colors.greenAccent[600],
+        //   borderRadius: 2,
+        //   borderColor: colors.greenAccent[600],
+        //   typography: "h4",
+        //   fontWeight: "bold",
+        //   '&:hover': {
+        //     color: colors.greenAccent[600],
+        //     backgroundColor: "white",
+        //     borderColor:  colors.greenAccent[600]
+        //   },
+        // }}
+        startIcon={<AddCircleSharpIcon />}
+        onClick={() => {
+          setSets(sets.concat(""));
+        }}
+        variant="outlined"
+      >
+        Newa
+      </Button>
+    </Box>
+  );
+};
+export default function enterWorkout() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
-  const alist = [{id: 'a', name: "aaaa"}]
+  const alist = [{ id: "a", name: "aaaa" }];
 
   const [exercises, setExercises] = useState(alist);
 
-    return(<Box m="20px" flex="1">
-    <Box display="flex" sx={{ flexDirection: "row" }}>
-      <Box
+  return (
+    <Box flex="1" alignItems="center" mb="15px">
+      <Typography
         flex="1"
-        mr="20px"
-        height="83vh"
+        align="center"
+        variant="h2"
+        color={colors.grey[100]}
+        fontWeight="bold"
+      >
+        Exercises
+      </Typography>
+      {exercises.map((item) => (
+        <ExerciseFields id={item.id} name={item.name} />
+      ))}
+      <Button
+        flex="1"
+        align="right"
         sx={{
-          "& .MuiDataGrid-root": { border: "none", fontSize: "1.25rem" },
-          "& .MuiDataGrid-cell": { border: "none" },
-          "& .name-column-cell": { border: "none", fontWeight: "bold" },
-          "& .MuiDataGrid-columnHeaders": {
-            backgroundColor: colors.primary[600],
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-footerContainer": {
-            borderTop: "none",
-            backgroundColor: colors.primary[600],
+          color: "black",
+          backgroundColor: colors.greenAccent[600],
+          borderRadius: 2,
+          borderColor: colors.greenAccent[600],
+          typography: "h4",
+          fontWeight: "bold",
+          "&:hover": {
+            color: colors.greenAccent[600],
+            backgroundColor: "white",
+            borderColor: colors.greenAccent[600],
           },
         }}
+        startIcon={<AddCircleSharpIcon />}
+        onClick={() => {
+          setExercises(exercises.concat({ id: "a", name: "aaaa" }));
+        }}
+        // variant="outlined"
       >
-        <Box
-          flex="1"
-          display="flex"
-          sx={{ flexDirection: "row" }}
-          alignItems="center"
-          mb="15px"
-        >
-          <Typography
-            justifyContent="center"
-            flex="1"
-            align="center"
-            variant="h2"
-            color={colors.grey[100]}
-            fontWeight="bold"
-          >
-            Exercises
-          </Typography>
-          <Button
-            flex="1"
-            align="right"
-            sx={{
-              color: "black",
-              backgroundColor: colors.greenAccent[600],
-              borderRadius: 2,
-              borderColor: colors.greenAccent[600],
-              typography: "h4",
-              fontWeight: "bold",
-              '&:hover': {
-                color: colors.greenAccent[600],
-                backgroundColor: "white", 
-                borderColor:  colors.greenAccent[600]
-              },
-            }}
-            startIcon={<AddCircleSharpIcon />}
-            onClick={() => {
-              setExercises(exercises.concat({id: 'a', name: "aaaa"}))
-            }}
-           // variant="outlined"
-          >
-            New
-          </Button>
-        </Box>
-        {exercises.map((item) => (
-          <ExerciseFields id={item.id} name ={item.name}/>
-      ))}
-      </Box>
-      <Divider
-        flexItem={true}
-        orientation="vertical"
-        flex="1"
-        sx={{ bgcolor: colors.grey[300] }}
-      />
+        New
+      </Button>
     </Box>
-    
-  </Box>)}
-
+  );
+}
